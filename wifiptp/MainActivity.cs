7d86316743fe6800c256d8a3dd2cd18b2b124a72
Shinyi Chen <jchen@isi.edu>
2017-08-17 17:21:53 -0700

    initial commit

---
+++ wifiptp/MainActivity.cs
@@ -0,0 +1,93 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using static Android.Net.Wifi.P2p.WifiP2pManager;
+using Android.Net.Wifi.P2p;
+using Android.Content;
+using System.Collections.Generic;
+using Android.Runtime;
+using System.Linq;
+using Android.Util;
+
+namespace wifiptp
+{
+    [Activity(Label = "wifiptp", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity, IActionListener, IPeerListListener
+	{
+
+		private WifiP2pManager wifiManager;
+		private Channel channel;
+		private BroadcastReceiver wifiBroadcastReceiver;
+
+		private IntentFilter intentFilter;
+		private List<WifiP2pDevice> devices;
+
+		private Button searchButton;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			SetContentView(Resource.Layout.Main);
+			searchButton = FindViewById<Button>(Resource.Id.discoverButton);
+			searchButton.Click += delegate {
+				searchButton.Enabled = false;
+				discover();
+			};
+
+			wifiManager = (WifiP2pManager)GetSystemService(Context.WifiP2pService);
+			channel = wifiManager.Initialize(this, MainLooper, null);
+			wifiBroadcastReceiver = new WiFiDirectBroadcastReceiver(wifiManager, channel, this);
+
+			intentFilter = new IntentFilter();
+			intentFilter.AddAction(WifiP2pManager.WifiP2pStateChangedAction);
+			intentFilter.AddAction(WifiP2pManager.WifiP2pPeersChangedAction);
+			intentFilter.AddAction(WifiP2pManager.WifiP2pConnectionChangedAction);
+			intentFilter.AddAction(WifiP2pManager.WifiP2pThisDeviceChangedAction);
+		}
+
+		protected override void OnResume()
+		{
+			base.OnResume();
+			RegisterReceiver(wifiBroadcastReceiver, intentFilter);
+		}
+
+		protected override void OnPause()
+		{
+			base.OnPause();
+			UnregisterReceiver(wifiBroadcastReceiver);
+		}
+
+		private void discover()
+		{
+			// discover peers
+			wifiManager.DiscoverPeers(channel, this);
+		}
+
+		// IActionListener: discover peers fail
+		public void OnFailure([GeneratedEnum] WifiP2pFailureReason reason)
+		{
+
+		}
+
+		// IActionListener: discover peers success
+		public void OnSuccess()
+		{
+
+		}
+
+		// IPeerListListener: peers found
+		public void OnPeersAvailable(WifiP2pDeviceList peers)
+		{
+			searchButton.Enabled = true;
+			devices = peers.DeviceList.ToList();
+
+			// TODO show devices in list
+			foreach (WifiP2pDevice device in devices)
+			{
+				Log.Info("WiFiActivity", "Device: " + device.DeviceAddress);
+			}
+		}
+	}
+}
+
---
+++ wifiptp/WiFiDirectBroadcastReceiver.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Net.Wifi.P2p;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using static Android.Net.Wifi.P2p.WifiP2pManager;
+
+namespace wifiptp
+{
+    [BroadcastReceiver(Enabled = true, Exported = false)]
+    public class WiFiDirectBroadcastReceiver : BroadcastReceiver
+    {
+        private WifiP2pManager manager;
+        private Channel channel;
+        private MainActivity activity;
+
+        public WiFiDirectBroadcastReceiver(WifiP2pManager manager, Channel channel, MainActivity activity)
+        {
+            this.manager = manager;
+            this.channel = channel;
+            this.activity = activity;
+        }
+
+        public override void OnReceive(Context context, Intent intent)
+        {
+            //Toast.MakeText(context, "Received intent!", ToastLength.Short).Show();
+            string action = intent.Action;
+            if (action.Equals(WifiP2pManager.WifiP2pStateChangedAction))
+            {
+                int state = intent.GetIntExtra(WifiP2pManager.ExtraWifiState, -1);
+                if (state == WifiP2pState.Enabled.GetHashCode())
+                {
+                    // WIFI enabled
+                }
+                else
+                {
+                    // WIFI disabled
+                }
+            }
+            else if (action.Equals(WifiP2pManager.WifiP2pPeersChangedAction))
+            {
+                // Call WifiP2pManager.requestPeers() to get a list of current peers
+                // Discover was called and found
+                if (manager != null)
+                    manager.RequestPeers(channel, (IPeerListListener)activity);
+            }
+            else if (action.Equals(WifiP2pManager.WifiP2pConnectionChangedAction))
+            {
+                // Respond to new connection or disconnections
+            }
+            else if (action.Equals(WifiP2pManager.WifiP2pThisDeviceChangedAction))
+            {
+                // Respond to this device's wifi state changing
+            }
+        }
+    }
+}
