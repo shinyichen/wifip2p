6dc6ecf07272b39d1995a13cf0b212de7ffeebae
Shinyi Chen <jchen@isi.edu>
2017-09-26 11:14:50 -0700

    Start P2pService when device boots

---
+++ wifiptp/BootCompletedReceiver.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace wifiptp
+{
+    
+    public class BootCompletedReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            // Start p2p service at device startup
+            Intent i = new Intent(context, typeof(P2pService));
+            context.StartService(i);
+        }
+    }
+}
